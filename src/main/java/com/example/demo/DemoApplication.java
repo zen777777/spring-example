fac693b68304014c9775405f7461a8f011b76f30
HONDA Zen <kd1300216@st.kobedenshi.ac.jp>
2023-12-26 20:15:01 +0900

    更新

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
     }
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("こんにちは %s!", name);
+      return String.format("こんにちは %s!",  name);
 
     }
 }
